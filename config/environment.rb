56b55afb5b42887349baf150c6d4e5554296eabc
Joseph <jvg_0119@yahoo.com>
2017-06-02 15:35:45 -0700

    modify environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,13 +4,16 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
+if Rails.env.development? || Rails.env.production?
 
-ActionMailer::Base.smtp_settings = {
-  :user_name => 'app69851461@heroku.com',
-  :password => 'kbuyasce5771',
-  :domain => 'https://blocipedia3-joe.herokuapp.com/',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
+  ActionMailer::Base.smtp_settings = {
+    :user_name => 'app69851461@heroku.com',
+    :password => 'kbuyasce5771',
+    :domain => 'https://blocipedia3-joe.herokuapp.com/',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+
+end
