e3e4f9996ce455589d9dae44cbff13c53ede8aa2
Joseph <jvg_0119@yahoo.com>
2017-06-01 18:24:14 -0700

    first commit

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,23 @@
+module ApplicationHelper
+
+  def active?(path)
+		"active" if current_page?(path)
+	end
+
+  def flash_class(key)
+    case key
+      when "success" then "alert-success"
+      when "info" then "alert-info"
+      when "warning" then "alert-warning"
+      when "notice" then "alert-warning"
+      when "danger" then "alert-danger"
+      when "error" then "alert-danger"
+    end
+  end
+
+end
+
+
+
+# flash_class(key)
+# alert-success
