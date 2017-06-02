96ce44ff103586d25573a7b6e010a4e0e34852db
Joseph <jvg_0119@yahoo.com>
2017-06-02 16:39:29 -0700

    config/env

--- config/environment.rb
+++ config/environment.rb
@@ -2,14 +2,14 @@
 require_relative 'application'
 
 # Initialize the Rails application.
-Rails.application.initialize!
 
+Rails.application.initialize!
 if Rails.env.development? || Rails.env.production?
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     :user_name => 'app69851461@heroku.com',
     :password => 'kbuyasce5771',
-    :domain => 'https://blocipedia3-joe.herokuapp.com/',
+    :domain => 'blocipedia3-joe.herokuapp.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
