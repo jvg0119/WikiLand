2a1a2538652f83e716264b472654683069d5d92b
Joseph <jvg_0119@yahoo.com>
2017-06-02 10:44:07 -0700

    add devise, sendgrid, figaro and deploy to heroku

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'app69851461@heroku.com',
+  :password => 'kbuyasce5771',
+  :domain => 'https://blocipedia3-joe.herokuapp.com/',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
