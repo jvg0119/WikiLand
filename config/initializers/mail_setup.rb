129fdda8a89e3e8dd3e0884a8b878a2a99925bc0
Joseph <jvg_0119@yahoo.com>
2017-06-02 17:47:17 -0700

    another commit

---
+++ config/initializers/mail_setup.rb
@@ -0,0 +1,14 @@
+
+if Rails.env.development? || Rails.env.production?
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'blocipedia3-joe.herokuapp.com',
+    :address => 'smtp.sendgrid.net',
+    :port => '587',
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+
+end
